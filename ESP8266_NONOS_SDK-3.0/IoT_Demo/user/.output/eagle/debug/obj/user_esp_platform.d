7109154827e2b8d6ac5a66e44f46106514b9e009
ld007 <332969359@qq.com>
2019-08-05 09:43:20 +0800

    添加工程文件： 1.实现了对乐鑫云的连接 2.实现了SNTP服务

---
+++ ESP8266_NONOS_SDK-3.0/IoT_Demo/user/.output/eagle/debug/obj/user_esp_platform.d
@@ -0,0 +1,25 @@
+.output/eagle/debug/obj/user_esp_platform.o .output/eagle/debug/obj/user_esp_platform.d : user_esp_platform.c ../../include/sntp.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/ip_addr.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/time.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/newlib.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/config.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/reent.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/lock.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/time.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/features.h \
+ ../../include/ets_sys.h ../../include/os_type.h ../../include/mem.h \
+ ../../include/osapi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/string.h \
+ ../include/user_config.h ../../include/user_interface.h \
+ ../../include/queue.h ../../include/spi_flash.h ../../include/gpio.h \
+ ../../include/espconn.h ../include/user_esp_platform.h \
+ ../include/user_iot_version.h ../include/user_config.h \
+ ../../include/upgrade.h ../include/user_light.h ../../include/pwm.h
---
+++ ESP8266_NONOS_SDK-3.0/IoT_Demo/user/user_main.c
@@ -0,0 +1,159 @@
+/*
+ * ESPRESSIF MIT License
+ *
+ * Copyright (c) 2016 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+ *
+ * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
+ * it is free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "ets_sys.h"
+#include "osapi.h"
+
+#include "user_interface.h"
+
+#include "user_devicefind.h"
+#include "user_webserver.h"
+#include "uart.h"
+#if ESP_PLATFORM
+#include "user_esp_platform.h"
+#endif
+
+#if ((SPI_FLASH_SIZE_MAP == 0) || (SPI_FLASH_SIZE_MAP == 1))
+#error "The flash map is not supported"
+#elif (SPI_FLASH_SIZE_MAP == 2)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x81000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0xfb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0xfc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0xfd000
+#define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR           0x7c000
+#elif (SPI_FLASH_SIZE_MAP == 3)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x81000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x1fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x1fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x1fd000
+#define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR           0x7c000
+#elif (SPI_FLASH_SIZE_MAP == 4)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x81000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x3fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x3fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x3fd000
+#define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR           0x7c000
+#elif (SPI_FLASH_SIZE_MAP == 5)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x101000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x1fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x1fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x1fd000
+#define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR           0xfc000
+#elif (SPI_FLASH_SIZE_MAP == 6)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x101000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x3fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x3fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x3fd000
+#define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR           0xfc000
+#else
+#error "The flash map is not supported"
+#endif
+
+#define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM                SYSTEM_PARTITION_CUSTOMER_BEGIN
+
+uint32 priv_param_start_sec;
+
+static const partition_item_t at_partition_table[] = {
+    { SYSTEM_PARTITION_BOOTLOADER, 						0x0, 												0x1000},
+    { SYSTEM_PARTITION_OTA_1,   						0x1000, 											SYSTEM_PARTITION_OTA_SIZE},
+    { SYSTEM_PARTITION_OTA_2,   						SYSTEM_PARTITION_OTA_2_ADDR, 						SYSTEM_PARTITION_OTA_SIZE},
+    { SYSTEM_PARTITION_RF_CAL,  						SYSTEM_PARTITION_RF_CAL_ADDR, 						0x1000},
+    { SYSTEM_PARTITION_PHY_DATA, 						SYSTEM_PARTITION_PHY_DATA_ADDR, 					0x1000},
+    { SYSTEM_PARTITION_SYSTEM_PARAMETER, 				SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR, 			0x3000},
+    { SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM,             SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR,          0x1000},
+};
+
+void ICACHE_FLASH_ATTR user_pre_init(void)
+{
+    if(!system_partition_table_regist(at_partition_table, sizeof(at_partition_table)/sizeof(at_partition_table[0]),SPI_FLASH_SIZE_MAP)) {
+		os_printf("system_partition_table_regist fail\r\n");
+		while(1);
+	}
+}
+#if 0  // for test  rename ap ssid  password
+void ICACHE_FLASH_ATTR
+ user_init(void){
+ struct softap_config config;//首先定义一个soft_config的结构体
+    uint8 opmode;
+
+    uart_init(115200,115200);
+    wifi_set_opmode(0x03);//设置为AP模式
+    opmode = wifi_get_opmode_default();
+    os_printf("\r\n\r\n\r\n当前的工作模式:%d\r\n",opmode);
+
+    wifi_softap_get_config(&config);//这个函数的参数是soft_config的结构体，记得取地址
+
+    os_memcpy(config.ssid,ESP8266_SSID,strlen(ESP8266_SSID));//
+    os_memcpy(config.password,ESP8266_PASSWORD,strlen(ESP8266_PASSWORD));//
+    config.ssid_len = strlen(ESP8266_SSID); 
+    wifi_softap_set_config(&config);//完成AP模式的参数配置，记得取地址
+}
+#else
+/******************************************************************************
+ * FunctionName : user_init
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
+void ICACHE_FLASH_ATTR
+user_init(void)
+{
+    partition_item_t partition_item;
+    uart_init(BIT_RATE_115200, BIT_RATE_115200);
+    os_printf("SDK version:%s\n", system_get_sdk_version());
+    os_printf("\r\n-------------------ld007 --hello word!!----------------------------\r\n");
+    os_printf("---------ld007 rgb wc light demo  sntp  20190801 14:56------------------\r\n");
+
+    if (!system_partition_get_item(SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM, &partition_item)) {
+        os_printf("Get partition information fail\n");
+    }
+    priv_param_start_sec = partition_item.addr/SPI_FLASH_SEC_SIZE;
+#if ESP_PLATFORM
+    /*Initialization of the peripheral drivers*/
+    /*For light demo , it is user_light_init();*/
+    /* Also check whether assigned ip addr by the router.If so, connect to ESP-server  */
+    user_esp_platform_init();
+#endif
+    /*Establish a udp socket to receive local device detect info.*/
+    /*Listen to the port 1025, as well as udp broadcast.
+    /*If receive a string of device_find_request, it rely its IP address and MAC.*/
+    user_devicefind_init();
+
+    /*Establish a TCP server for http(with JSON) POST or GET command to communicate with the device.*/
+    /*You can find the command in "2B-SDK-Espressif IoT Demo.pdf" to see the details.*/
+    /*the JSON command for curl is like:*/
+    /*3 Channel mode: curl -X POST -H "Content-Type:application/json" -d "{\"period\":1000,\"rgb\":{\"red\":16000,\"green\":16000,\"blue\":16000}}" http://192.168.4.1/config?command=light      */
+    /*5 Channel mode: curl -X POST -H "Content-Type:application/json" -d "{\"period\":1000,\"rgb\":{\"red\":16000,\"green\":16000,\"blue\":16000,\"cwhite\":3000,\"wwhite\",3000}}" http://192.168.4.1/config?command=light      */
+#ifdef SERVER_SSL_ENABLE
+    user_webserver_init(SERVER_SSL_PORT);
+#else
+    user_webserver_init(SERVER_PORT);
+#endif
+}
+#endif
